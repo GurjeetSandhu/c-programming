46bbaa7a1b09d416cd551f2f80128f05f45eb12b
Coursera Learner <nobody@nowhere.nul>
2020-08-14 18:09:20 +0000

    Done retirement assignment

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -20,7 +20,7 @@ void retirement(int startAge, double initial, retire_info working, retire_info r
       if(i<=483)
 	{
 	  if(i==0) {
-	    initial = (initial*working.rate_of_return)+ initial;
+	    initial = (initial*working.rate_of_return*i)+ initial;
 	   printf("Age %3d months %2d you have $%.2lf\n", j, k, initial);
 	  }
 	   else
