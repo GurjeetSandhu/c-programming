46cee6a403e0c9514f16ba2d8627adbacbd37b4c
Gurjeet Singh <65786117+GurjeetSandhu@users.noreply.github.com>
2020-08-18 12:38:07 +0530

    Update my-test-main.c

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,10 @@
 #include "cards.h"
-
+#include <stdio.h>
 int main(void) {
-
-}
+  card_t gurjeet = {7, SPADES};
+  char a = value_letter(gurjeet);
+  char b = suit_letter(gurjeet) ;
+  print_card(gurjeet);
+  card_t temp = card_from_letters(gurjeet.value, gurjeet.suit);
+  printf("%c%c",a,b);
+  } 
