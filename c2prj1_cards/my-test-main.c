ca88ebfbeaa0167975c9ed55d469140e4b051096
Coursera Learner <nobody@nowhere.nul>
2020-08-18 07:38:59 +0000

    Assignment

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -5,6 +5,5 @@ int main(void) {
   char a = value_letter(gurjeet);
   char b = suit_letter(gurjeet) ;
   print_card(gurjeet);
-  card_t temp = card_from_letters(gurjeet.value, gurjeet.suit);
-  printf("%c%c",a,b);
-  } 
+  printf("%c %c",a,b);
+   } 
